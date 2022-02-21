4bc6b277dca080ef7827ecbaba6f9bfdf0735aa5
Aris Papazisis <arpapaz1984@gmail.com>
2022-02-21 21:18:18 +0200

    Intro

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace ConsoleApp19
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
